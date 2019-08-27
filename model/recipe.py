d0a70a305f2a01fe9618217255b435085cdbb2b0
Floréal Cabanettes <floreal.cab@flo-art.fr>
2019-08-27 20:16:12 +0200

    Improve recipe model

--- model/recipe.py
+++ model/recipe.py
@@ -6,14 +6,14 @@ import datetime
 class Ingredient(EmbeddedDocument):
     name = StringField(max_length=100, required=True)
     is_group_title = BooleanField(required=False, default=False)
-    quantity = IntField(required=False)
-    unit = StringField(max_length=100, required=False)
-    level = IntField(default=0)
+    quantity = IntField(required=False, null=True)
+    unit = StringField(max_length=100, required=False, null=True)
+    level = IntField(default=0, required=False)
 
 
 class Instruction(EmbeddedDocument):
     text_inst = StringField(required=True)
-    level = IntField(default=0)
+    level = IntField(default=0, required=False)
 
 
 class Proposal(EmbeddedDocument):
@@ -41,15 +41,15 @@ class Recipe(Document):
     proposals = EmbeddedDocumentListField(Proposal, required=False, default=[])
     excerpt = StringField(required=True)
     time_prep = IntField(required=True)
-    time_cook = IntField(required=False)
-    time_rest = IntField(required=False)
+    time_cook = IntField(required=False, null=True)
+    time_rest = IntField(required=False, null=True)
     picture_file = StringField(required=True)
     nb_people = IntField(required=True)
-    nb_people_max = IntField(required=False)
-    precision = StringField(max_length=150, required=False)
+    nb_people_max = IntField(required=False, null=True)
+    precision = StringField(max_length=150, required=False, null=True)
     enable_comments = BooleanField(required=False, default=True)
     published = BooleanField(required=False, default=False)
     slug = StringField(max_length=255, required=True, unique=True)
-    heart_stroke = IntField(choices=[1, 2, 3], required=False)
+    heart_stroke = IntField(choices=[1, 2, 3], required=False, null=True)
     categories = EmbeddedDocumentListField(Category, required=True)
 
